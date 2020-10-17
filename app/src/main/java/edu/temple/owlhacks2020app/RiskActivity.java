a8c428e92fd363cb0bcde39481ace5f1bd690f31
Alan Freeman <freemana@ieee.org>
2020-10-17 15:59:51 -0400

    4:00

---
+++ app/src/main/java/edu/temple/owlhacks2020app/RiskActivity.java
@@ -0,0 +1,14 @@
+package edu.temple.owlhacks2020app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RiskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_risk);
+    }
+}
